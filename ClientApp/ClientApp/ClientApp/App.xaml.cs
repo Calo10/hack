22b6a9beadfa512e72c3e582b8916d1d03bfe3fc
stvansolano <stvansolano@gmail.com>
2016-08-27 16:15:36 -0600

    Actualizacion de la app y correciones

---
+++ ClientApp/ClientApp/ClientApp/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿[assembly: Xamarin.Forms.Xaml.XamlCompilation(Xamarin.Forms.Xaml.XamlCompilationOptions.Compile)]
+namespace ClientApp
+{
+    using Xamarin.Forms;
+    using Pages;
+
+    public partial class App :Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            Root = new MainPage();
+            TheMapPage = new MapPage();
+            MainNavigationPage = new NavigationPage(Root);
+            MainPage = MainNavigationPage;
+        }
+
+        public NavigationPage MainNavigationPage { get; set; }
+        public MainPage Root { get; private set; }
+        public MapPage TheMapPage { get; private set; }
+       
+        /*
+        protected async override void OnStart()
+        {
+            await TheMapPage.SetupMap();
+        }*/
+    }
+}
---
+++ ClientApp/ClientApp/ClientApp/Pages/PickPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace ClientApp.Pages
+{
+    public partial class PickPage : ContentPage
+    {
+        public PickPage()
+        {
+            InitializeComponent();
+
+            /*
+            if (CrossMedia.Current.IsCameraAvailable && CrossMedia.Current.IsTakePhotoSupported)
+            {
+                // Supply media options for saving our photo after it's taken.
+                var mediaOptions = new Plugin.Media.Abstractions.StoreCameraMediaOptions
+                {
+                    Directory = "Pickups",
+                    Name = $"{DateTime.UtcNow}.jpg"
+                };
+
+                // Take a photo of the business receipt.
+                var file = await CrossMedia.Current.TakePhotoAsync(mediaOptions);
+            }*/
+        }
+    }
+}
