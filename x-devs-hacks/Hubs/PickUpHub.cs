61f18304c3991042996616c8b09a16624552a870
Carlos Mendez <carlos_mendez_10@live.com>
2016-08-27 11:00:11 -0600

    SignalR Implemented

---
+++ x-devs-hacks/Hubs/PickUpHub.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace x_devs_hacks.Hubs
+{
+    public class PickUpHub : Hub
+    {
+        public void Hello()
+        {
+            Clients.All.hello();
+        }
+    }
+}
