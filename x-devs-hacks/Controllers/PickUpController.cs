6132936892824b0b264248d48b8f3c1905e07b85
Carlos Mendez <carlos_mendez_10@live.com>
2016-08-27 11:51:43 -0600

    PickUps Hard Coded getAll

---
+++ x-devs-hacks/Controllers/PickUpController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Script.Serialization;
+using x_devs_hacks.Models;
+
+namespace x_devs_hacks.Controllers
+{
+    public class PickUpController : Controller
+    {
+        //
+        // GET: /PickUpRequest/
+        PickUpModel pickUpM = new PickUpModel();
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public string getAllPickUps() {
+
+            JavaScriptSerializer jSS = new JavaScriptSerializer();
+
+            List<PickUpModel> lstPickUps = pickUpM.getAllPickUps();
+
+            return jSS.Serialize(lstPickUps);
+        
+        }
+
+    }
+}
